64c18e743d33f6fc699908066c375ee2c13b0ec8
Ertinfagor <ertinfagor@users.noreply.github.com>
2017-06-15 21:41:13 +0300

    add steemit post spider

---
+++ templateproject/templateproject/spiders/steemit_post.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+
+from scrapy_splash import SplashRequest
+
+class SteemitPostSpider(scrapy.Spider):
+    name = 'steemit_post'
+    allowed_domains = ['steemit.com']
+    start_urls = ['https://steemit.com/@ertinfagor/blog']
+    # http_user = 'splash-user'
+    # http_pass = 'splash-password'
+
+    def parse(self, response):
+        le = LinkExtractor()
+        for link in le.extract_links(response):
+            yield SplashRequest(
+                link.url,
+                self.parse_link,
+                endpoint='render.json',
+                args={
+                    'har': 1,
+                    'html': 1,
+                }
+            )
+
+    def parse_link(self, response):
+        print("PARSED", response.real_url, response.url)
+        print(response.css("title").extract())
+        print(response.data["har"]["log"]["pages"])
+        print(response.headers.get('Content-Type'))
